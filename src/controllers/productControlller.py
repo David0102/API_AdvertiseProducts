01deafbb6bec64fb94ecca445ef5739b7d0c7f85
David0102 <davidmoura0102@gmail.com>
2023-09-13 19:41:46 -0300

    add route products create

--- src/controllers/productControlller.py
+++ src/controllers/productControlller.py
@@ -1,5 +1,8 @@
 from sqlalchemy.orm import Session
 from src.models.product import Product
+from src.models.user import User
+from fastapi import UploadFile
+import uuid
 
 class ProductController:
     def __init__(self, db: Session):
@@ -8,3 +11,34 @@ class ProductController:
     def products_list_all(self):
         products = self.db.query(Product).all()
         return products
+    
+    def create(self, user: User, name: str, description: str, price: float, category_id: int, image: UploadFile):
+        image_url = self.image_save(image)
+
+        product = Product(
+            name = name,
+            description = description,
+            price = price,
+            image_url = image_url,
+            usuario_id = user.id,
+            category_id = category_id
+        )
+
+        product_create = self.save_product(product)
+        return product_create
+    
+    def image_save(self, image: UploadFile):
+        image.filename = f"{uuid.uuid4()}.png"
+
+        image_url = f'http://localhost:8000/staticfiles/products/{image.filename}'
+
+        with open(f"staticfiles/products/{image.filename}", "wb") as image_file:
+            image_file.write(image.file.read())
+        
+        return image_url
+    
+    def save_product(self, product: Product):
+        self.db.add(product)
+        self.db.commit()
+        self.db.refresh(product)
+        return product
--- src/routes/product_router.py
+++ src/routes/product_router.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form, UploadFile, File
 from src.database.config import get_db
 from sqlalchemy.orm import Session
 from src.controllers.productControlller import ProductController
 from src.schemas.schemas import ProductSchemaResponse
 from typing import List
+from src.authorization.authorization import get_current_user
+from src.models.user import User
 
 router = APIRouter(prefix='/products')
 
@@ -11,3 +13,15 @@ router = APIRouter(prefix='/products')
 def products_list(db: Session = Depends(get_db)):
     products = ProductController(db).products_list_all()
     return products
+
+@router.post('/create', response_model=ProductSchemaResponse, status_code=201)
+def product_create(user: User = Depends(get_current_user),
+    name: str = Form(),
+    description: str = Form(),
+    price: float = Form(),
+    category_id: int = Form(),
+    image: UploadFile = File(),
+    db: Session = Depends(get_db)):
+
+    product = ProductController(db).create(user, name, description, price, category_id, image)
+    return product
